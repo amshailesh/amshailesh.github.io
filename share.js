cbc321cf4bbdc2e9f06b450590e0e38c984d85cd
amshailesh <56260446+amshailesh@users.noreply.github.com>
2022-08-30 14:33:02 +0530

    Create share.js

---
+++ share.js
@@ -0,0 +1,6 @@
+
+<script>
+if (screen && screen.width < 600) {
+  document.write('<script type="text/javascript" src=" https://platform-api.sharethis.com/js/sharethis.js#property=5f05c4b91bbdc90012dbcf4e&product=inline-share-buttons async='async' "><\/script>');
+}
+</script>
