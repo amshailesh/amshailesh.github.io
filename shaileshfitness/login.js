88bc8a0e26de15e1017bb4252c7237305c551d61
amshailesh <56260446+amshailesh@users.noreply.github.com>
2022-09-01 20:25:02 +0530

    ifuture practice html
    ifuture practice html

---
+++ shaileshfitness/login.js
@@ -0,0 +1,66 @@
+
+
+function chkPassword(){
+var vUsername;
+	if(vUsername != "" && document.getElementById("txtUsername").value!="null" && document.getElementById("txtPassword").value!="null" && document.getElementById("txtUsername").value==document.getElementById("txtUsername").value && document.getElementById("txtPassword").value=="ifuture"){
+		vUsername=document.getElementById("txtUsername").value;
+
+		//alert("before cookie set");
+		localStorage.setItem("username", vUsername);
+		//alert("cookie set");
+		//swal("Great","Correct Credentials","success");
+
+		window.location.href="landing.html";
+		
+	}
+	else{
+		swal("You Entered Wrong Credentials","Please enter username & password as ifuture only","error");
+	}
+}
+
+function chkResetPassword(){
+
+	if(document.getElementById("txtUsername").value!="null" && document.getElementById("txtPassword").value!="null" && document.getElementById("txtUsername").value=="ifuture" && document.getElementById("txtPassword").value=="ifuture"){
+		swal("Great","Correct Credentials","success");
+		window.location.href="landing.html";
+	}
+	else{
+		swal("You Entered Wrong Credentials","Please enter username & secret question answer as ifuture only","error");
+	}
+}
+
+
+function remItem(){
+
+	localStorage.removeItem("username");
+	//localStorage.removeItem("fsubmit");
+	window.location.href="login.html";
+}
+
+function checkCookie() {
+
+
+//localStorage.setItem("fsubmit","datareceived");
+
+  let username = localStorage.getItem("username");
+
+  //if(fsubmit !="" && fsubmit!=null){
+  //	continue;
+ // }
+   if (username != "" && username !=null) {
+   document.getElementById("greet").innerHTML=""+username+" Welcome to Members Page";
+  } else {
+    
+    window.location.href="login.html";
+    }
+  }
+
+  function checkCookieLogin() {
+  let username = localStorage.getItem("username");
+  if (username != "" && username !=null) {
+   window.location.href="landing.html";
+  } else {
+    
+    //window.location.href="login.html";
+    }
+  }
